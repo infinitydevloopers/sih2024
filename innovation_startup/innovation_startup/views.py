d6cfac754a18ee11255dc16326de8439575dac45
ceo-szhoes <aj045045@gmail.com>
2024-09-04 13:37:33 +0530

    Add Django Project

---
+++ innovation_startup/innovation_startup/views.py
@@ -0,0 +1,12 @@
+from  django.http import  HttpResponse,  HttpResponseRedirect
+from django.shortcuts import render
+
+def homepage(request):
+
+    return render(request,"index.html")
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def signup_view(request):
+    return render(request, 'signup.html')
